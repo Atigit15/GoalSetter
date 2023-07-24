f0a951914e644bc2aeedebdd32b7be2e23f0a2a6
Atishay Jain <Atigit15@users.noreply.github.com>
2023-07-24 22:37:13 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
@@ -13,6 +14,11 @@ const app = express();
 //middleware so that we can access the req.body in POST reqs
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin : ["https://localhost:3000"],
+  })
+)
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
